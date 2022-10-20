c30ed408fc4aa2b8c3c72ca7db2b739454c93ffc
WANNA959 <wannazjx@163.com>
2022-10-20 14:22:23 +0800

    add vendor

---
+++ pkg/utils/virsh_utils_test.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestGetPoolInfo(t *testing.T) {
+	//info, err := GetPoolInfo("pooltest131-2")
+	//fmt.Printf("info: %+v\n", info)
+	//fmt.Printf("err: %+v\n", err)
+}
