8382573535b19156fa168375dc82e331f5eef2e6
WANNA959 <wannazjx@163.com>
2022-11-02 19:35:30 +0800

    pass delete external snapshot

---
+++ test/snapshot/test-delete-domain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+virsh destroy test111
+virsh undefine test111
+
+kubectl apply -f ../vm/01-CreateVMFromISO.json
+sleep 5
+virsh start test111
+
+rm -rf /var/lib/libvirt/pooltest111/disktest111/snapshots
+cat << EOF > /var/lib/libvirt/pooltest111/disktest111/config.json
+{"current":"/var/lib/libvirt/pooltest111/disktest111/disktest111.qcow2","dir":"/var/lib/libvirt/pooltest111/disktest111","name":"disktest111","pool":"pooltest111"}
+EOF
+
+go build -o /tmp/sdsctl /root/go_project/sdsctl/cmd/sdsctl/main.go
+yes | cp  /tmp/sdsctl /usr/bin
+
+kubectl delete vmdsn disktest111-snapshot2 disktest111-snapshot
+sleep 2
+kubectl apply -f 01-CreateExternalSnapshot.json
+sleep 3
+kubectl apply -f 01-CreateExternalSnapshot-2.json
+sleep 2
+kubectl get vmdsn
---
+++ test/snapshot/test-delete-nodomain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+virsh destroy test111
+virsh undefine test111
+
+rm -rf /var/lib/libvirt/pooltest111/disktest111/snapshots
+cat << EOF > /var/lib/libvirt/pooltest111/disktest111/config.json
+{"current":"/var/lib/libvirt/pooltest111/disktest111/disktest111.qcow2","dir":"/var/lib/libvirt/pooltest111/disktest111","name":"disktest111","pool":"pooltest111"}
+EOF
+
+go build -o /tmp/sdsctl /root/go_project/sdsctl/cmd/sdsctl/main.go
+yes | cp  /tmp/sdsctl /usr/bin
+
+kubectl delete vmdsn disktest111-snapshot2 disktest111-snapshot
+sleep 2
+kubectl apply -f 01-CreateExternalSnapshot.json
+sleep 3
+kubectl apply -f 01-CreateExternalSnapshot-2.json
+sleep 2
+kubectl get vmdsn
